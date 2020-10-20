dc276d8fd136d5cdc81857b14a4a511e4046fab0
Claire Meyer <clairemeyerbourhis@gmail.com>
2020-10-20 21:01:13 +0200

    added : Initial repository commit (functional limited troll bot).

---
+++ index.js
@@ -0,0 +1,35 @@
+var Discord = require("discord.js");
+var bot = new Discord.Client();
+var isReady = true;
+
+bot.on(
+	"message", message => {
+		if (isReady && message.content === "oh dear") {
+			isReady = false;
+			console.log(bot.channels.cache)
+			// that one specific channel on that one specific server :
+			//var voiceChannel = bot.channels.cache.get("622846233903628362");
+
+			// voice channel general on server hosting the text channel from which the message originates
+			var voiceChannel = message.guild.channels.cache.find((value,key,collection)=>value.name=="General" && value.type=="voice");
+
+			// voice channel on which message author is connected 
+
+			// voice channel on which tagged user is connected
+
+			voiceChannel.join().then(
+				connection => {
+					const dispatcher = connection.play("./troll.mp4");
+					dispatcher.on(
+						"end", end => {
+							voiceChannel.leave();
+						}
+					);
+				}
+			).catch(err => console.log(err));
+			isReady = true;
+		}
+	}
+);
+
+bot.login(<HERE BE BOT KEY>);
